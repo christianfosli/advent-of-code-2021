011b9d662391a1e80e38581dac177b83d52515b9
Christian Fosli <christian.fosli@capgemini.com>
2021-12-03 21:15:38 +0100

    refactor(day3-1): remove unneeded enum and half-broken fold function

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,83 +1,39 @@
-use std::fmt;
+use std::cmp::Ordering;
 
-#[derive(Copy, Clone, Debug)]
-enum MostCommonBit {
-    High(u16),
-    Low(u16),
-    Equally,
-}
+fn find_gamma(report: &[String]) -> Result<u16, anyhow::Error> {
+    let mut counts: Vec<isize> = vec![0; report[0].len()];
 
-impl fmt::Display for MostCommonBit {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            MostCommonBit::High(_) => write!(f, "1"),
-            MostCommonBit::Low(_) => write!(f, "0"),
-            MostCommonBit::Equally => write!(f, ""),
+    for number in report {
+        for (pos, bit) in number.chars().enumerate() {
+            match bit {
+                '0' => counts[pos] -= 1,
+                '1' => counts[pos] += 1,
+                _ => anyhow::bail!("invalid format - non binary"),
+            };
         }
     }
-}
 
-fn find_gamma_and_epsilon(report: &[String], test: bool) -> Result<(u16, u16), anyhow::Error> {
-    let most_common_bits = report
-        .into_iter()
-        .fold([MostCommonBit::Equally; 12], |mut acc, x| {
-            for (pos, bit) in x.chars().enumerate() {
-                let new_x = match bit {
-                    '0' => match acc[pos] {
-                        MostCommonBit::High(n) => {
-                            if n == 1 {
-                                MostCommonBit::Equally
-                            } else {
-                                MostCommonBit::High(n - 1)
-                            }
-                        }
-                        MostCommonBit::Low(n) => MostCommonBit::Low(n + 1),
-                        MostCommonBit::Equally => MostCommonBit::Low(1),
-                    },
-                    '1' => match acc[pos] {
-                        MostCommonBit::High(n) => MostCommonBit::High(n + 1),
-                        MostCommonBit::Low(n) => {
-                            if n == 1 {
-                                MostCommonBit::Equally
-                            } else {
-                                MostCommonBit::Low(n - 1)
-                            }
-                        }
-                        MostCommonBit::Equally => MostCommonBit::High(1),
-                    },
-                    _ => unreachable!(""),
-                };
-
-                acc[pos] = new_x;
-            }
-
-            acc
-        });
-
-    let gamma = most_common_bits
+    let gamma: String = counts
         .iter()
-        .fold("".to_owned(), |acc, x| format!("{}{}", acc, x));
+        .map(|c| match c.cmp(&0) {
+            Ordering::Less => '0',    // majority of bits are low
+            Ordering::Greater => '1', // majority of bits are high
+            Ordering::Equal => panic!("not sure how to handle when no bit 'wins'"),
+        })
+        .collect();
 
-    let gamma = u16::from_str_radix(&gamma, 2)?;
-
-    let epsilon = if test {
-        !gamma & 0b0000000000001111
-    } else {
-        !gamma & 0b0000111111111111
-    };
-
-    Ok((gamma, epsilon))
+    Ok(u16::from_str_radix(&gamma, 2)?)
 }
 
 fn main() -> Result<(), anyhow::Error> {
-    let report: Vec<String> = std::fs::read_to_string("input.txt")?
+    let diagnostics_report: Vec<String> = std::fs::read_to_string("input.txt")?
         .lines()
         .map(String::from)
         .collect();
 
-    if let Ok((gamma, epsilon)) = find_gamma_and_epsilon(&report, false) {
-        println!("{}", u64::from(gamma) * u64::from(epsilon));
-    }
+    let gamma = find_gamma(&diagnostics_report)?;
+    let epsilon = !gamma & 0b0000111111111111;
+    println!("part 1: {}", u32::from(gamma) * u32::from(epsilon));
 
     Ok(())
 }
@@ -87,7 +43,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_passes_aoc_testcase() {
+    fn it_passes_aoc_testcase_1() -> Result<(), anyhow::Error> {
         let diagnostic_report: Vec<_> = vec![
             "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000",
             "11001", "00010", "01010",
@@ -96,10 +52,9 @@ mod tests {
         .map(String::from)
         .collect();
 
-        if let Ok((gamma, epsilon)) = find_gamma_and_epsilon(&diagnostic_report, true) {
-            assert_eq!(198, u64::from(gamma) * u64::from(epsilon));
-        } else {
-            panic!("find_gamma_and_epsilon returned an error :-(");
-        }
+        let gamma = find_gamma(&diagnostic_report)?;
+        let epsilon = !gamma & 0b0000000000001111;
+        assert_eq!(198, gamma * epsilon);
+        Ok(())
     }
 }
