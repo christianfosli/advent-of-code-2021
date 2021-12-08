4b63171320c2d2f4c63fe57c0b7f8a1be95f47a4
Christian Fosli <christian.fosli@capgemini.com>
2021-12-08 22:34:35 +0100

    [wip] try to do day08 part2 but didnt get time to finish off

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 /// part 1 - count 1, 4, 7 or 8 in output signal
 fn count_easy_digits_in_output(signals: &[String]) -> usize {
     let is_1 = |digit: &str| digit.len() == 2;
@@ -15,6 +17,61 @@ fn count_easy_digits_in_output(signals: &[String]) -> usize {
         })
 }
 
+/// part 2 -- DOES NOT WORK YET!
+fn add_output_values(signals: &[String]) -> usize {
+    signals.iter().map(|s| find_output(&s)).sum()
+}
+
+fn find_output(signals: &String) -> usize {
+    if let [signals, output] = signals.split(" | ").collect::<Vec<_>>()[..] {
+        let mut translations: HashMap<char, char> = HashMap::new();
+
+        let c_or_f: HashSet<char> = signals
+            .split(' ')
+            .find(|d| d.len() == 2)
+            .unwrap()
+            .chars()
+            .collect();
+
+        let f = c_or_f
+            .iter()
+            .find(|&&x| {
+                signals
+                    .split(' ')
+                    .all(|digit| digit.chars().any(|c| c == x))
+            })
+            .unwrap();
+
+        translations.insert('f', *f);
+
+        translations.insert(
+            'c',
+            *(&c_or_f - &HashSet::from([*f])).iter().next().unwrap(),
+        );
+
+        let a = signals
+            .split(' ')
+            .find(|d| d.len() == 3) // 4: a or c or f
+            .unwrap()
+            .chars()
+            .find(|&x| x != *f && x != translations[&'c'])
+            .unwrap();
+
+        translations.insert('a', a);
+
+        let _b_c_d_or_f: HashSet<char> = signals
+            .split(' ')
+            .find(|d| d.len() == 4)
+            .unwrap()
+            .chars()
+            .collect();
+
+        // TODO: Keep working out what letter map to what
+        return 0;
+    }
+    panic!("invalid format");
+}
+
 fn main() -> Result<(), anyhow::Error> {
     let signals: Vec<String> = std::fs::read_to_string("input.txt")?
         .lines()
@@ -22,6 +79,13 @@ fn main() -> Result<(), anyhow::Error> {
         .collect();
 
     println!("part 1: {}", count_easy_digits_in_output(&signals));
+
+    println!(
+        "{:?}",
+        signals.iter().all(|s| s.split(' ').any(|d| d.len() == 3))
+    );
+
+    println!("part 2: {}", add_output_values(&signals));
 
     Ok(())
 }
@@ -46,5 +110,11 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
     fn it_passes_aoc_testcase_1() {
         let signals: Vec<String> = EXAMPLE_SIGNALS.lines().map(String::from).collect();
         assert_eq!(26, count_easy_digits_in_output(&signals));
+    }
+
+    #[test]
+    fn it_passes_aoc_testcase_2() {
+        let signals: Vec<String> = EXAMPLE_SIGNALS.lines().map(String::from).collect();
+        assert_eq!(61_229, add_output_values(&signals));
     }
 }
