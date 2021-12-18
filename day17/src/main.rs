9c1c2bc64d95f4f6240794b34eab65f876576f22
Christian Fosli <christian.fosli@capgemini.com>
2021-12-18 19:19:40 +0100

    implement day17 part 2, refactor part 1

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,22 +1,25 @@
 use std::{cmp::Ordering, ops::Range};
 
-const TARGET_AREA: (Range<i32>, Range<i32>) = (207..264, -115..-64);
+const TARGET_AREA: (Range<i32>, Range<i32>) = (207..264, -115..-62);
 
 /// Part 1
-/// Finds the initial velocity that causes the proab to reach the highet y position
+/// Finds the initial velocity that causes the proab to reach the highest y position
 /// and still eventually be within the target area after any step
 fn find_best_velocity(target: &(Range<i32>, Range<i32>)) -> Option<((i32, i32), i32)> {
-    let mut best: Option<((i32, i32), i32)> = None;
-    for x in 0..100 {
-        for y in -100..300 {
-            if let Some(hit) = launch((x, y), target) {
-                if best.is_none() || hit > best.unwrap().1 {
-                    best = Some(((x, y), hit));
-                }
-            }
-        }
-    }
-    best
+    (0..100)
+        .flat_map(|x| (-100..300).map(move |y| (x, y)))
+        .filter_map(|vel| {
+            let high = launch(vel, target)?;
+            Some((vel, high))
+        })
+        .max_by(|(_, h1), (_, h2)| h1.cmp(h2))
+}
+
+fn count_velocities(target: &(Range<i32>, Range<i32>)) -> usize {
+    (0..500)
+        .flat_map(|x| (-300..300).map(move |y| (x, y)))
+        .filter_map(|vel| launch(vel, target))
+        .count()
 }
 
 /// If trajectory hits target area -> returns Some(highest y position)
@@ -25,7 +28,7 @@ fn launch(velocity: (i32, i32), target: &(Range<i32>, Range<i32>)) -> Option<i32
     // Ugh, a lot of mutable state here. Couldn't think of a functional solution...
     let (mut x_vel, mut y_vel) = velocity;
     let (mut x, mut y) = (0, 0);
-    let mut highest_y = 0;
+    let mut highest_y = i32::min_value();
     let mut hit_target = false;
 
     while y_vel > 0 || y > target.1.start {
@@ -57,16 +60,22 @@ fn launch(velocity: (i32, i32), target: &(Range<i32>, Range<i32>)) -> Option<i32
 
 fn main() {
     println!("part 1: {:?}", find_best_velocity(&TARGET_AREA));
+    println!("part 2: {:?}", count_velocities(&TARGET_AREA));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TARGET_AREA: (Range<i32>, Range<i32>) = (20..31, -10..-6);
+    const TARGET_AREA: (Range<i32>, Range<i32>) = (20..31, -10..-4);
 
     #[test]
     fn it_passes_aoc_testcase_1() {
-        assert_eq!(Some(((6, 9), 45)), find_best_velocity(&TARGET_AREA));
+        assert_eq!(45, find_best_velocity(&TARGET_AREA).unwrap().1);
+    }
+
+    #[test]
+    fn it_passes_aoc_testcase_2() {
+        assert_eq!(112, count_velocities(&TARGET_AREA));
     }
 }
