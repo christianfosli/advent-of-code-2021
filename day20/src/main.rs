850437e194611323b387ec113f2732b3d2eed31c
Christian Fosli <christian.fosli@capgemini.com>
2021-12-21 08:08:07 +0100

    fix day20 part1, implement day20 part 2. TODO: Clean up some repeating code

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,41 +1,41 @@
 use std::{error::Error, iter};
 
-fn enhance(algo: &str, img: &str) -> String {
+fn enhance(algo: &str, img: &str, outside: char) -> (String, char) {
     let width = img.lines().next().unwrap().len() + 6;
     let height = img.lines().count() + 6;
 
     // Add some padding
-    let img: String = vec!['.'; width]
+    let img: String = vec![outside; width]
         .into_iter()
         .chain(iter::once('\n'))
-        .chain(vec!['.'; width].into_iter())
+        .chain(vec![outside; width].into_iter())
         .chain(iter::once('\n'))
-        .chain(vec!['.'; width].into_iter())
+        .chain(vec![outside; width].into_iter())
         .chain(iter::once('\n'))
         .chain(
             img.lines()
-                .map(|l| format!("...{}...", l))
+                .map(|l| {
+                    format!(
+                        "{}{}{}{}{}{}{}",
+                        outside, outside, outside, l, outside, outside, outside
+                    )
+                })
                 .collect::<Vec<_>>()
                 .join("\n")
                 .chars(),
         )
         .chain(iter::once('\n'))
-        .chain(vec!['.'; width].into_iter())
+        .chain(vec![outside; width].into_iter())
         .chain(iter::once('\n'))
-        .chain(vec!['.'; width].into_iter())
+        .chain(vec![outside; width].into_iter())
         .chain(iter::once('\n'))
-        .chain(vec!['.'; width].into_iter())
+        .chain(vec![outside; width].into_iter())
         .chain(iter::once('\n'))
         .collect();
 
-    let mut output_img: Vec<char> = Vec::with_capacity(width * height + height);
-
-    // Handle first row
-    output_img.extend_from_slice(&vec!['.'; width]);
-    output_img.push('\n');
+    let mut output_img: Vec<char> = Vec::with_capacity(width * height);
 
     for y in 1..(height - 1) {
-        output_img.push('.'); // first col
         for x in 1..(width - 1) {
             let index = img
                 .lines()
@@ -57,13 +57,15 @@ fn enhance(algo: &str, img: &str) -> String {
 
             output_img.push(algo.chars().nth(index.into()).unwrap());
         }
-        output_img.push('.'); // last col
         output_img.push('\n');
     }
-    output_img.extend_from_slice(&vec!['.'; width]); // handle last row specially
-    output_img.push('\n');
 
-    output_img.iter().collect()
+    let new_outside = algo
+        .chars()
+        .nth(if outside == '.' { 0 } else { 0b1_1111_1111 })
+        .unwrap();
+
+    (output_img.iter().collect(), new_outside)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -71,11 +73,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let algo = input.lines().next().unwrap();
     let img = input.split("\n\n").nth(1).unwrap();
     println!("img:\n{}", &img);
-    let enhanced = enhance(algo, img);
+    let (enhanced, outside) = enhance(algo, img, '.');
     println!("enhanced:\n{}", &enhanced);
-    let enhanced = enhance(algo, &enhanced);
-    println!("enhanced:\n{}", &enhanced);
+    let (enhanced, _) = enhance(algo, &enhanced, outside);
+    println!("enhanced x2:\n{}", &enhanced);
     println!("part 1: {}", enhanced.chars().filter(|&c| c == '#').count());
+
+    // My solution is a bit slow for part 2 -- Run with `--release` and wait a few seconds :-)
+    let (p2, _) = (0..50).fold((img.to_owned(), '.'), |(img, out), _| {
+        enhance(algo, &img, out)
+    });
+    println!("part 2: {}", p2.chars().filter(|&c| c == '#').count());
 
     Ok(())
 }
@@ -95,9 +103,9 @@ mod tests {
     #[test]
     fn it_enhances_image() {
         println!("initial:\n{}", PUZZLE);
-        let enhanced = enhance(IMG_ENHANCEMENT_ALGO, PUZZLE);
+        let (enhanced, outside) = enhance(IMG_ENHANCEMENT_ALGO, PUZZLE, '.');
         println!("enhanced:\n{}", &enhanced);
-        let enhanced = enhance(IMG_ENHANCEMENT_ALGO, &enhanced);
+        let (enhanced, _) = enhance(IMG_ENHANCEMENT_ALGO, &enhanced, outside);
         println!("enhanced x2:\n{}", &enhanced);
     }
 }
